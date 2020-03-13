802774e7a8551473c4b2078575e42b18909e6e8a
natek <natekinderman16@gmail.com>
2020-03-12 21:50:49 -0500

    Added Js function ageInDays to Click me button and it appends to flex-box-result div tag

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,11 @@
-//alert("hello")
+function ageInDays(){
+    var birthYear = prompt("What year were you born?")
+    var totalDaysAlive = (2020 - birthYear) *365;
+    var h1 = document.createElement("h1");
+    var textAnswer = document.createTextNode('You are ' + totalDaysAlive + ' days old!')
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-results').appendChild(h1);
+
+}
+
